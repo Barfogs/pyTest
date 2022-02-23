0bc78fedb924ccc123500d7c1b5cf2cf5844b374
Barfogs <88886083+Barfogs@users.noreply.github.com>
2022-02-23 20:17:54 +0300

    Delete test directory

--- test/test_items.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-import time
-def test_button(browser):
-    browser.implicitly_wait(5)
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket"), "Кнопка корзины отсутствует"
