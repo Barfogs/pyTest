6943bc996828d08b46568cc3c4afdd3f97a7b44b
Barfogs <88886083+Barfogs@users.noreply.github.com>
2022-02-23 20:12:52 +0300

    Update test_items.py

--- test/test_items.py
+++ test/test_items.py
@@ -3,5 +3,4 @@ import time
 def test_button(browser):
     browser.implicitly_wait(5)
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    time.sleep(30)
     assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket"), "Кнопка корзины отсутствует"
