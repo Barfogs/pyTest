2fb1823f19a421df37d7c7ce42d7b0b8591925bd
Barfogs <88886083+Barfogs@users.noreply.github.com>
2022-02-23 20:25:34 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-def test_button(browser):
+def test_find_button(browser):
     browser.implicitly_wait(5)
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket"), "Кнопка корзины отсутствует"
