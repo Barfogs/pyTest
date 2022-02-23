0cd653dfb5da35268a05881e7eee50aa81f3f9b7
Barfogs <88886083+Barfogs@users.noreply.github.com>
2022-02-23 20:21:53 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="ru",
-                     help="Введите язык интерфейсы: ru, es, en, fr...")
+                     help="Введите язык интерфейса: ru, es, en, fr...")
 
 
 @pytest.fixture(scope="function")
